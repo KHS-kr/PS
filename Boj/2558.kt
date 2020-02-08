eea7c168af55d2e5abbf75b06a7b0bc1f01bd25d
KHS-kr <59349226+KHS-kr@users.noreply.github.com>
2020-02-08 16:02:28 +0900

    Create 2558.kt

---
+++ Boj/2558.kt
@@ -0,0 +1,9 @@
+import java.util.*
+
+fun main(args : Array<String>) {
+    var scanner : Scanner = Scanner(System.`in`)
+    var A = scanner.nextInt()
+    var B = scanner.nextInt()
+
+    println((A + B))
+}
