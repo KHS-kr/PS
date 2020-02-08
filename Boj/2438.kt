c35f6ef84e018b268d34d476f9693d0cb7368a58
KHS-kr <59349226+KHS-kr@users.noreply.github.com>
2020-02-08 16:01:02 +0900

    Create 2438.kt

---
+++ Boj/2438.kt
@@ -0,0 +1,12 @@
+import java.util.*
+
+fun main(args : Array<String>) {
+    var scanner : Scanner = Scanner(System.`in`)
+    var A = scanner.nextInt()
+
+    for(i : Int in 1..A) {
+        for (j: Int in 1..i)
+            print("*")
+        println()
+    }
+}
