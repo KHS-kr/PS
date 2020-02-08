4b15168e79d29c0c1f95b129742e5b68fcdbf25f
KHS-kr <59349226+KHS-kr@users.noreply.github.com>
2020-02-08 16:00:24 +0900

    Create 2440.kt

---
+++ Boj/2440.kt
@@ -0,0 +1,12 @@
+import java.util.*
+
+fun main(args : Array<String>) {
+    var scanner : Scanner = Scanner(System.`in`)
+    var A = scanner.nextInt()
+
+    for(i in (1..A).reversed()) {
+        for(j in 1..i)
+            print("*")
+        println()
+    }
+}
