7826535d2a84841b9bcc56edf6fe6dee333f5be8
KHS-kr <59349226+KHS-kr@users.noreply.github.com>
2020-02-08 16:03:45 +0900

    Create 1001.kt

---
+++ Boj/1001.kt
@@ -0,0 +1,14 @@
+import java.util.*
+
+fun main(args : Array<String>) {
+    var scanner : Scanner = Scanner(System.`in`)
+    var A = scanner.nextInt()
+    var B = scanner.nextInt()
+    
+    print(A op B)
+}
+
+//infix fun Int.plus(other : Int) = this + other
+infix fun Int.op(other : Int): Int {
+    return this - other
+}
