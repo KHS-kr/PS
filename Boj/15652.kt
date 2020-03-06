eb6abc2d59ebbb73a37d7de873b35ef4f54f4bf6
KHS-kr <59349226+KHS-kr@users.noreply.github.com>
2020-03-06 16:10:25 +0900

    Create 15652.kt

---
+++ Boj/15652.kt
@@ -0,0 +1,33 @@
+import java.util.*
+
+lateinit var temp : Array<Int>
+lateinit var visited : Array<Boolean>
+var N = 0; var M = 0
+var ret = arrayOf<Array<Int>>()
+
+fun main() {
+    val sc = Scanner(System.`in`)
+    N = sc.nextInt()
+    M = sc.nextInt()
+
+    temp = Array(M) { 0 }
+    visited = Array(N) { false }
+
+    dfs(0)
+}
+
+fun dfs(n : Int) {
+    if(n == M) {
+        for(i in temp)
+            print("$i ")
+        println()
+    }
+    else {
+        for(i in 1..N) {
+            if(n != 0 && temp[n - 1] > i)
+                continue
+            temp[n] = i
+            dfs(n + 1)
+        }
+    }
+}
