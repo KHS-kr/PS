ceb52dd521605c1626cf2e085c67ce948128217b
KHS-kr <59349226+KHS-kr@users.noreply.github.com>
2020-03-09 14:29:15 +0900

    Create 2531.kt

---
+++ Boj/2531.kt
@@ -0,0 +1,45 @@
+import java.util.*
+
+val arr = Array(33333) { 0 }
+val slide = Array(3333) { 0 }
+var diff = 0 //종류
+var eat = 0 //먹은
+
+fun main() {
+    val sc = Scanner(System.`in`)
+    val N = sc.nextInt()
+    val d = sc.nextInt()
+    val k = sc.nextInt()
+    val c = sc.nextInt()
+    val cnt = -1
+    var ret = 0
+
+    for(i in 0 until N)
+        arr[i] = sc.nextInt()
+
+    slide[c] = 1
+    diff++
+
+    for(i in 0 until k) {
+        if(slide[arr[i]] == 0) diff++
+        slide[arr[i]]++
+        eat++
+
+        arr[N + i] = arr[i]
+    }
+    for(i in k until k + N) {
+        if(ret < diff)
+            ret = diff
+
+        if (slide[arr[i]] == 0)
+            diff++
+        slide[arr[i]]++
+        eat++
+        if (slide[arr[i - k]] == 1)
+            diff--
+        slide[arr[i - k]]--
+        eat++
+    }
+
+    println(ret)
+}
