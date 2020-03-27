10f8c1e0c36f2df5db72ba939fbbaf5ec4a5a76c
KHS-kr <59349226+KHS-kr@users.noreply.github.com>
2020-03-27 14:57:28 +0900

    Create 11053.kt

---
+++ Boj/11053.kt
@@ -0,0 +1,26 @@
+import java.util.*
+
+var N = 0
+lateinit var arr : IntArray
+val dp = IntArray(1000) { 1 }
+
+fun main() = with(Scanner(System .`in`)) {
+    N = nextInt()
+    arr = IntArray(N) { nextInt() }
+
+    var ret = 0
+
+    for(i in 0 until N) {
+        var min = 0
+        for(j in 0 until i) {
+            if(arr[i] > arr[j])
+                if(min < dp[j])
+                    min = dp[j]
+        }
+        dp[i] = min + 1
+        if(ret < dp[i])
+            ret = dp[i]
+    }
+
+    println(ret)
+}
