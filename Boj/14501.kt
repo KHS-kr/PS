5421b69f77be4743a129cdd77a468e4a3c7ed864
KHS-kr <59349226+KHS-kr@users.noreply.github.com>
2020-03-31 16:37:17 +0900

    Create 14501.kt

---
+++ Boj/14501.kt
@@ -0,0 +1,25 @@
+import java.util.*
+import kotlin.math.max
+
+var N = 0
+val t = IntArray(16)
+val p = IntArray(16)
+val dp = IntArray(16)
+
+fun main() = with(Scanner(System.`in`)) {
+    N = nextInt()
+    repeat(N) {
+        t[it + 1] = nextInt()
+        p[it + 1] = nextInt()
+        dp[it + 1] = p[it + 1]
+    }
+
+    for(i in 2..N) {
+        for(j in 1 until i) {
+            if(i - j >= t[j])
+                dp[i] = max(p[i] + dp[j], dp[i])
+        }
+    }
+
+    println(dp.max())
+}
