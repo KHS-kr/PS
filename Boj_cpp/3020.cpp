435bedbc60b28972668bab5bf85a0219801b36ae
KHS-kr <59349226+KHS-kr@users.noreply.github.com>
2020-06-01 10:30:49 +0900

    Create 3020.cpp

---
+++ Boj_cpp/3020.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstdio>
+#include <queue>
+#include <cstring>
+#include <regex>
+#include <climits>
+#include <cmath>
+#include <cstdlib>
+#include <stack>
+
+using namespace std;
+
+int N, H, ret = INT_MAX, cnt;
+vector<int> top, bottom;
+
+int main() {
+    cin >> N >> H;
+    
+    top.resize(N / 2);
+    bottom.resize(N / 2);
+    
+    for(int i = 0; i < N / 2; i++) {
+        cin >> bottom[i] >> top[i];
+    }
+    
+    sort(bottom.begin(), bottom.end());
+    sort(top.begin(), top.end());
+    
+    cnt = 1;
+    
+    for(int i = 1; i < H; i++) {
+        int temp = bottom.size() - (lower_bound(bottom.begin(), bottom.end(), i) - bottom.begin());
+        temp += top.size() - (upper_bound(top.begin(), top.end(), H - i) - top.begin());
+        
+        if(ret == temp)
+            cnt++;
+        else if(ret > temp) {
+            ret = temp;
+            cnt = 1;
+        }
+    }
+    
+    cout << ret << " " << cnt << endl;
+}
